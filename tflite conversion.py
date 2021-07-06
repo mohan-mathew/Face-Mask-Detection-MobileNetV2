6777f96e0a285b9d13af471da8816d892b48d591
mohan-mathew <66155035+mohan-mathew@users.noreply.github.com>
2021-07-06 20:21:22 +0530

    Add files via upload

---
+++ tflite conversion.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+model = tf.keras.models.load_model('mask_detector.model')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+open("MDA.tflite", "wb").write(tflite_model)
